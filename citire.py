319cf5f743ced2563a45abd416a53e58be53ed7a
Lucian <ilielucian97@gmail.com>
2026-09-02 14:26:40 +0300

    chore: elimina scripturile legacy din proiectul activ

--- citire.py
+++
@@ -1,9 +0,0 @@
-with open("document1.txt", "r", encoding="utf-8") as fisier:
-    continut = fisier.read()
-
-print(continut)
-
-cuvinte = continut.split()
-numar_cuvinte = len(cuvinte)
-
-print(f"Documentul are {numar_cuvinte} cuvinte")
